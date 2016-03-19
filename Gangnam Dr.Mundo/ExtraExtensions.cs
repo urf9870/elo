7e22f1b64e6a02faa938f2a231af279aa57f6ddf
urf9870 <hyunsang638@gmail.com>
2016-03-19 18:48:18 +0900

    ?

---
+++ Gangnam Dr.Mundo/ExtraExtensions.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Linq;
+
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace Gangnam_Dr.Mundo
+{
+    internal static class ExtraExtensions
+    {
+        internal static bool IsReadyPerfectly(this Spell.SpellBase spell)
+        {
+            return spell != null && spell.Slot != SpellSlot.Unknown && spell.State != SpellState.Cooldown &&
+                   spell.State != SpellState.Disabled && spell.State != SpellState.NoMana &&
+                   spell.State != SpellState.NotLearned && spell.State != SpellState.Surpressed &&
+                   spell.State != SpellState.Unknown && spell.State != SpellState.Ready;
+        }
+
+        internal static bool IsKillableAndValidTarget(this AIHeroClient target,double calculatedDamage,
+            DamageType damegeType, float distance = float.MaxValue)
+        {
+            if (target == null || !target.IsValidTarget(distance) || target.CharData.BaseSkinName == "gangplankbarrel")
+            {
+                return false;
+            }
+
+            if (target.HasBuff("kindredrnodeathbuff"))
+            {
+                return false;
+            }
+
+            if (target.HasBuff("Undying Rage"))
+            {
+                return false;
+            }
+
+            if (target.HasBuff("JudicatorIntervention"))
+            {
+                return false;
+            }
+
+            if (target.HasBuff("BansheesVeil"))
+            {
+                return false;
+            }
+
+            if (target.HasBuff("SivirShield"))
+            {
+                return false;
+            }
+
+            if (target.HasBuff("ShroudofDarkness"))
+            {
+                return false;
+            }
+
+            if (ObjectManager.Player.HasBuff("summonerexhaust"))
+            {
+                calculatedDamage *= 0.6;
+            }
+
+            if (target.ChampionName == "Blitzcrank")
+            {
+                if (!target.HasBuff("manabarriercooldown"))
+                {
+                    if (target.Health + target.HPRegenRate + (damegeType == DamageType.Physical ? target.AttackShield : target.MagicShield) +
+                        target.Mana * 0.6 + target.PARRegenRate < calculatedDamage)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (target.ChampionName == "Garen")
+            {
+                if (target.HasBuff("GarenW"))
+                {
+                    calculatedDamage *= 0.7;
+                }
+            }
+
+            if (target.HasBuff("FerociousHowl"))
+            {
+                calculatedDamage *= 0.3;
+            }
+
+            return target.Health + target.HPRegenRate +
+                (damegeType == DamageType.Physical ? target.AttackShield : target.MagicShield) <
+                calculatedDamage - 2;
+        }
+
+        internal static bool IsKillableAndValidTarget(this Obj_AI_Minion target, double calculatedDamage,
+            DamageType damageType, float distance = float.MaxValue)
+        {
+            if (target == null || !target.IsValidTarget(distance) || target.Health <= 0 || 
+                target.HasBuffOfType(BuffType.SpellImmunity) || target.HasBuffOfType(BuffType.SpellShield) ||
+                target.CharData.BaseSkinName == "gangplankbarrel")
+            {
+                return false;
+            }
+
+            if (ObjectManager.Player.HasBuff("summonerexhaust"))
+            {
+                calculatedDamage *= 0.6;
+            }
+
+            var dragonslayerbuff = ObjectManager.Player.GetBuff("s5test_dragonslayerbuff");
+            if (dragonslayerbuff != null)
+            {
+                if (dragonslayerbuff.Count >= 4)
+                {
+                    calculatedDamage *= dragonslayerbuff.Count == 5 ? calculatedDamage * 0.30 : calculatedDamage * 0.15;
+                }
+
+                if (target.CharData.BaseSkinName.ToLowerInvariant().Contains("dragon"))
+                {
+                    calculatedDamage *= 1 - dragonslayerbuff.Count * 0.07;
+                }
+            }
+
+            if (target.CharData.BaseSkinName.ToLowerInvariant().Contains("baron") &&
+                ObjectManager.Player.HasBuff("barontarget"))
+            {
+                calculatedDamage *= 0.5;
+            }
+
+            return target.Health + target.HPRegenRate +
+                (damageType == DamageType.Physical ? target.AttackShield : target.MagicShield) <
+                calculatedDamage - 2;
+        }
+
+        internal static bool IsKillableAndValidTarget(this Obj_AI_Base target, double calculatedDamage,
+            DamageType damageType, float distance = float.MaxValue)
+        {
+            if (target == null || !target.IsValidTarget(distance) || target.CharData.BaseSkinName == "gangplankbarrel")
+                return false;
+
+            if (target.HasBuff("kindredrnodeathbuff"))
+            {
+                return false;
+            }
+
+            // Tryndamere's Undying Rage (R)
+            if (target.HasBuff("Undying Rage"))
+            {
+                return false;
+            }
+
+            // Kayle's Intervention (R)
+            if (target.HasBuff("JudicatorIntervention"))
+            {
+                return false;
+            }
+
+            // Poppy's Diplomatic Immunity (R)
+            if (target.HasBuff("DiplomaticImmunity") && !ObjectManager.Player.HasBuff("poppyulttargetmark"))
+            {
+                //TODO: Get the actual target mark buff name
+                return false;
+            }
+
+            // Banshee's Veil (PASSIVE)
+            if (target.HasBuff("BansheesVeil"))
+            {
+                // TODO: Get exact Banshee's Veil buff name.
+                return false;
+            }
+
+            // Sivir's Spell Shield (E)
+            if (target.HasBuff("SivirShield"))
+            {
+                // TODO: Get exact Sivir's Spell Shield buff name
+                return false;
+            }
+
+            // Nocturne's Shroud of Darkness (W)
+            if (target.HasBuff("ShroudofDarkness"))
+            {
+                // TODO: Get exact Nocturne's Shourd of Darkness buff name
+                return false;
+            }
+
+            if (ObjectManager.Player.HasBuff("summonerexhaust"))
+                calculatedDamage *= 0.6;
+
+            if (target.CharData.BaseSkinName == "Blitzcrank")
+                if (!target.HasBuff("manabarriercooldown"))
+                    if (target.Health + target.HPRegenRate +
+                        (damageType == DamageType.Physical ? target.AttackShield : target.MagicShield) +
+                        target.Mana * 0.6 + target.PARRegenRate < calculatedDamage)
+                        return true;
+
+            if (target.CharData.BaseSkinName == "Garen")
+                if (target.HasBuff("GarenW"))
+                    calculatedDamage *= 0.7;
+
+
+            if (target.HasBuff("FerociousHowl"))
+                calculatedDamage *= 0.3;
+
+            var dragonSlayerBuff = ObjectManager.Player.GetBuff("s5test_dragonslayerbuff");
+            if (dragonSlayerBuff != null)
+                if (target.IsMinion)
+                {
+                    if (dragonSlayerBuff.Count >= 4)
+                        calculatedDamage += dragonSlayerBuff.Count == 5 ? calculatedDamage * 0.30 : calculatedDamage * 0.15;
+
+                    if (target.CharData.BaseSkinName.ToLowerInvariant().Contains("dragon"))
+                        calculatedDamage *= 1 - dragonSlayerBuff.Count * 0.07;
+                }
+
+            if (target.CharData.BaseSkinName.ToLowerInvariant().Contains("baron") &&
+                ObjectManager.Player.HasBuff("barontarget"))
+                calculatedDamage *= 0.5;
+
+            return target.Health + target.HPRegenRate +
+                   (damageType == DamageType.Physical ? target.AttackShield : target.MagicShield) <
+                   calculatedDamage - 2;
+        }
+
+        internal static bool IsManaPercentOkey(this AIHeroClient hero, int manaPercent)
+        {
+            return hero.ManaPercent >= manaPercent;           
+        }
+
+        internal static bool IsHealthPercentOkey(this AIHeroClient hero,int healthPercent)
+        {
+            return hero.HealthPercent >= healthPercent;
+        }
+
+        internal static double IsImmobileUntil(this AIHeroClient unit)
+        {
+            var result =
+                unit.Buffs.Where(
+                    buff =>
+                        buff.IsActive && Game.Time <= buff.EndTime &&
+                        (buff.Type == BuffType.Charm || buff.Type == BuffType.Knockup || buff.Type == BuffType.Stun ||
+                         buff.Type == BuffType.Suppression || buff.Type == BuffType.Snare))
+                    .Aggregate(0d, (current, buff) => Math.Max(current, buff.EndTime));
+            return result - Game.Time;
+        }
+
+        internal static bool IsWillDieByTristanaE(this Obj_AI_Base target)
+        {
+            if (ObjectManager.Player.ChampionName == "Tristana")
+                if (target.HasBuff("tristanaecharge"))
+                    if (
+                        target.IsKillableAndValidTarget(
+                            (float)
+                                (ObjectManager.Player.GetSpellDamage(target, SpellSlot.E) *
+                                 (target.GetBuffCount("tristanaecharge") * 0.30) +
+                                 ObjectManager.Player.GetSpellDamage(target, SpellSlot.E)),
+                            DamageType.Physical))
+                        return true;
+            return false;
+        }
+
+        internal static float GetRealAutoAttackRange(this AttackableUnit unit, AttackableUnit target,
+    int autoAttackRange)
+        {
+            var result = autoAttackRange + unit.BoundingRadius;
+            if (target.IsValidTarget())
+                return result + target.BoundingRadius;
+            return result;
+        }
+    }
+}
---
+++ Gangnam Dr.Mundo/Program.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Linq;
+
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace Gangnam_Dr.Mundo
+{
+    class Program
+    {
+        private static Menu Menu, CMenu, HMenu, LCMenu, JCMenu, MMenu, DMenu;
+        private static Spell.Skillshot _q;
+        private static Spell.Active _w;
+        private static Spell.Active _e;
+        private static Spell.Active _r;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != "DrMundo")
+            {
+                return;
+            }
+
+            _q = new Spell.Skillshot(SpellSlot.Q, 1000, EloBuddy.SDK.Enumerations.SkillShotType.Linear, 250, 1850, 60) { MinimumHitChance = EloBuddy.SDK.Enumerations.HitChance.High };
+            _q.AllowedCollisionCount = 0;
+
+            _w = new Spell.Active(SpellSlot.W, 325);
+
+            _e = new Spell.Active(SpellSlot.E, 150);
+
+            _r = new Spell.Active(SpellSlot.R);
+
+            Menu = MainMenu.AddMenu("Dr.Mundo", "Ten Percent Dr.Mundo".ToLower(),"Ten Percent Dr.Mundo");
+            Menu.AddLabel("Gangnam");
+            Menu.AddLabel("Ten Percent Dr.Mundo Ver 0.0.0.9 (Beta)");
+            Menu.AddLabel("Last Update 2016.03.19");
+            Menu.AddLabel("Plz give me Many FeedBack :)");
+
+            CMenu = Menu.AddSubMenu("Combo", "CMenu");
+            CMenu.Add("CQ", new CheckBox("Use Q"));
+            CMenu.Add("CW", new CheckBox("Use W"));
+            CMenu.Add("CE", new CheckBox("Use E"));
+            CMenu.Add("CR", new CheckBox("Use R", false));
+            CMenu.Add("CH", new Slider("R If HP", 30, 0, 100));
+            CMenu.AddSeparator();
+
+            HMenu = Menu.AddSubMenu("Harass", "HMenu");
+            HMenu.Add("HQ", new CheckBox("Use Q"));
+            HMenu.Add("HW", new CheckBox("Use W"));
+            HMenu.Add("HE", new CheckBox("Use E"));
+            HMenu.AddSeparator();
+
+            LCMenu = Menu.AddSubMenu("LaneClear", "LCMenu");
+            LCMenu.Add("LCQ", new CheckBox("Use Q"));
+            LCMenu.Add("LCW", new CheckBox("Use W", false));
+            LCMenu.Add("LCE", new CheckBox("Use E"));
+            LCMenu.AddSeparator();
+
+            JCMenu = Menu.AddSubMenu("JungleClear", "JCMenu");
+            JCMenu.Add("JCQ", new CheckBox("Use Q"));
+            JCMenu.Add("JCW", new CheckBox("Use W", false));
+            JCMenu.Add("JCE", new CheckBox("Use E"));
+            JCMenu.AddSeparator();
+
+            MMenu = Menu.AddSubMenu("Misc", "MMenu");
+            MMenu.Add("MO", new Slider("Auto W Off If HP", 10, 0, 100));
+            MMenu.Add("MA", new CheckBox("Smart W Logic"));
+            MMenu.AddSeparator();
+
+            DMenu = Menu.AddSubMenu("Drawing", "DMenu");
+            DMenu.Add("DO", new CheckBox("Disable Drawings", false));
+            DMenu.Add("DQ", new CheckBox("Draw Q Range"));
+            DMenu.Add("DW", new CheckBox("Draw W Range"));
+            DMenu.Add("DH", new CheckBox("Draw HP"));
+            DMenu.AddSeparator();
+
+            Menu.AddSeparator();
+
+            Game.OnUpdate += Game_OnUpdate;
+            Orbwalker.OnPostAttack += Orbwalker_OnPostAttack;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+        
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (DMenu["DO"].Cast<CheckBox>().CurrentValue)
+            {
+                return;
+            }
+
+            if (DMenu["DQ"].Cast<CheckBox>().CurrentValue)
+            {
+                if (_q.IsReady())
+                {
+                    Circle.Draw(Color.Green, _q.Range, ObjectManager.Player.Position);
+                }
+            }
+
+            if (DMenu["DW"].Cast<CheckBox>().CurrentValue)
+            {
+                if (_w.IsReady())
+                {
+                    Circle.Draw(Color.Green, _w.Range, ObjectManager.Player.Position);
+                }
+            }
+
+            if (DMenu["DH"].Cast<CheckBox>().CurrentValue)
+            {
+                var pos = Drawing.WorldToScreen(Player.Instance.Position);
+                Drawing.DrawText(pos.X, pos.Y + 40, System.Drawing.Color.White, "HP : " + ObjectManager.Player.HealthPercent);
+            }
+        }
+
+        private static void Orbwalker_OnPostAttack(AttackableUnit target, EventArgs args)
+        {
+            if (target is AIHeroClient)
+            {
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+                {
+                    if (CMenu["CE"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_e.IsReady())
+                        {
+                            _e.Cast();
+                        }
+                    }
+                }
+                else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+                {
+                    if (HMenu["HE"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_e.IsReady())
+                        {
+                            _e.Cast();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (target is Obj_AI_Minion)
+                {
+                    if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+                    {
+                        if (LCMenu["LCE"].Cast<CheckBox>().CurrentValue)
+                        {
+                            if (_e.IsReady())
+                            {
+                                var t = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, 
+                                    ObjectManager.Player.Position, 150f).FirstOrDefault(x => x.IsValidTarget(150f));
+                                if (t != null)
+                                {
+                                    _e.Cast();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (JCMenu["JCE"].Cast<CheckBox>().CurrentValue)
+                {
+                    if (_e.IsReady())
+                    {
+                        var t = EntityManager.MinionsAndMonsters.GetJungleMonsters(ObjectManager.Player.Position, _q.Range).OrderBy(x => x.MaxHealth)
+                            .FirstOrDefault(x => x.IsValidTarget());
+
+                        if (t != null)
+                        {
+                            _e.Cast();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Orbwalker.CanMove)
+            {
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+                {
+                    if (CMenu["CQ"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_q.IsReady())
+                        {
+                            var target = TargetSelector.GetTarget(_q.Range, DamageType.Magical);
+                            if (target.IsValidTarget(_q.Range))
+                            {
+                                _q.Cast(target);
+                            }
+                        }
+                    }
+
+                    if (ObjectManager.Player.IsHealthPercentOkey(MMenu["MO"].Cast<Slider>().CurrentValue))
+                    {
+                        if (CMenu["CW"].Cast<CheckBox>().CurrentValue)
+                        {
+                            var target = TargetSelector.GetTarget(_w.Range + 20f, DamageType.Magical);
+
+                            if (target != null)
+                            {
+                                if (_w.Handle.ToggleState == 1)
+                                {
+                                    _w.Cast();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+                {
+                    if (HMenu["HQ"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_q.IsReady())
+                        {
+                            var target = TargetSelector.GetTarget(_q.Range, DamageType.Magical);
+                            if (target.IsValidTarget(_q.Range))
+                            {
+                                _q.Cast(target);
+                            }
+                        }
+                    }
+
+                    if (ObjectManager.Player.IsHealthPercentOkey(MMenu["MO"].Cast<Slider>().CurrentValue))
+                    {
+                        if (HMenu["HW"].Cast<CheckBox>().CurrentValue)
+                        {
+                            if (_w.IsReady())
+                            {
+                                var target = TargetSelector.GetTarget(_w.Range + 20f, DamageType.Magical);
+                                if (target != null)
+                                {
+                                    if (_w.Handle.ToggleState == 1)
+                                    {
+                                        _w.Cast();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+                {
+                    if (LCMenu["LCQ"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_q.IsReady())
+                        {
+                            var target = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, ObjectManager.Player.Position, _q.Range)
+                                .OrderBy(x => x.Health).FirstOrDefault(x => x.IsValidTarget(_q.Range));
+
+                            if (target != null)
+                            {
+                                if (target.BaseSkinName != "ward")
+                                {
+                                    _q.Cast(target);
+                                }
+                            }
+                        }
+                    }
+
+                    if (ObjectManager.Player.IsHealthPercentOkey(MMenu["MO"].Cast<Slider>().CurrentValue))
+                    {
+                        if (LCMenu["LCW"].Cast<CheckBox>().CurrentValue)
+                        {
+                            if (_w.IsReady())
+                            {
+                                var target = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy,
+                                    ObjectManager.Player.Position, _w.Range + 20f).ToList();
+
+                                if (target.Count >= 1)
+                                {
+                                    if (_w.Handle.ToggleState == 1)
+                                    {
+                                        _w.Cast();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+                {
+                    if (JCMenu["JCQ"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_q.IsReady())
+                        {
+                            var target = EntityManager.MinionsAndMonsters.GetJungleMonsters(ObjectManager.Player.Position, _q.Range)
+                                .OrderBy(x => x.MaxHealth).FirstOrDefault(x => x.IsValidTarget(_q.Range));
+
+                            if (target != null)
+                            {
+                                _q.Cast(target);
+                            }
+                        }
+                    }
+
+                    if (ObjectManager.Player.IsHealthPercentOkey(MMenu["MO"].Cast<Slider>().CurrentValue))
+                    {
+                        if (JCMenu["JCW"].Cast<CheckBox>().CurrentValue)
+                        {
+                            if (_w.IsReady())
+                            {
+                                var target = EntityManager.MinionsAndMonsters.GetJungleMonsters(ObjectManager.Player.Position, _q.Range)
+                                    .OrderBy(x => x.MaxHealth).FirstOrDefault(x => x.IsValidTarget(_w.Range + 20f));
+
+                                if (target != null)
+                                {
+                                    if (_w.Handle.ToggleState == 1)
+                                    {
+                                        _w.Cast();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (MMenu["MA"].Cast<CheckBox>().CurrentValue)
+                {
+                    if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+                    {
+                        var miniontarget = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy,
+                            ObjectManager.Player.Position, _w.Range + 100f).ToList();
+                        var jungletarget = EntityManager.MinionsAndMonsters.GetJungleMonsters(ObjectManager.Player.Position, _w.Range + 200f).OrderBy(x => x.MaxHealth).ToList();
+
+                        if (miniontarget.Count < 1 && jungletarget.Count < 1)
+                        {
+                            if (_w.Handle.ToggleState != 1)
+                            {
+                                _w.Cast();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+                        {
+                            var target = EntityManager.MinionsAndMonsters.GetJungleMonsters(ObjectManager.Player.Position, _w.Range + 200f).OrderBy(x => x.MaxHealth).ToList();
+
+                            if (target.Count < 1)
+                            {
+                                if (_w.Handle.ToggleState != 1)
+                                {
+                                    _w.Cast();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            var target = TargetSelector.GetTarget(float.MaxValue, DamageType.Magical);
+                            if (target != null)
+                            {
+                                if (target.Position.Distance(ObjectManager.Player.Position) >= 599.5f)
+                                {
+                                    if (_w.Handle.ToggleState != 1)
+                                    {
+                                        _w.Cast();
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (_w.Handle.ToggleState != 1)
+                                {
+                                    _w.Cast();
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            if (!(ObjectManager.Player.IsHealthPercentOkey(CMenu["CH"].Cast<Slider>().CurrentValue)))
+            {
+                if (!ObjectManager.Player.IsRecalling())
+                {
+                    if (CMenu["CR"].Cast<CheckBox>().CurrentValue)
+                    {
+                        if (_r.IsReady())
+                        {
+                            _r.Cast();
+                        }
+                    }
+                }
+            }
+        }      
+    }
+}
